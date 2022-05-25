2889408082258816ab263c5c7118030eda8dd805
antonloginchev@gmail.com <antonloginchev@gmail.com>
2022-05-25 10:02:46 +0300

    DTO

--- dto/models.go
+++ dto/models.go
@@ -1,12 +1,33 @@
-package main
+package dto
 
-type Task struct {
-	Idtasks     uint
-	Description string
+type User struct {
+	Id          string `json"id,omitempty" bson"id,omitempty"`
+	DisplayName string `json:"displayname,omitempty" bson:"displayname,omitempty"`
+	URL         string `json:"url,omitempty" bson:"url,omitempty"`
 }
 
-type Comment struct {
-	Idcomments uint
-	Idtasks    uint
-	Text       string
+type TaskStatus struct {
+	Id          string `json"id,omitempty" bson"id,omitempty"`
+	Name        string `json:"name,omitempty" bson:"name,omitempty"`
+	StatusGroup string `json:"statusGroup,omitempty" bson:"statusGroup,omitempty"`
+}
+
+type Task struct {
+	Id        string `json"id,omitempty" bson"id,omitempty"`
+	Status    string `json:"status,omitempty" bson:"status,omitempty"`
+	Creator   string `json:"creator,omitempty" bson:"creator,omitempty"`
+	Performer string `json:"performer,omitempty" bson:"performer,omitempty"`
+}
+
+type Task2Target struct {
+	Task   string `json"task,omitempty" bson"task,omitempty"`
+	Target string `json"target,omitempty" bson"target,omitempty"`
+}
+
+type Target struct {
+	Id             string `json"id,omitempty" bson"id,omitempty"`
+	Name           string `json:"name,omitempty" bson:"name,omitempty"`
+	ExpectedResult int    `json:"expectedResult,omitempty" bson:"expectedResult,omitempty"`
+	TargetGroup    string `json:"targetGroup,omitempty" bson:"targetGroup,omitempty"`
+	Creator        string `json:"creator,omitempty" bson:"creator,omitempty"`
 }
